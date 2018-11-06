ef2f0a9a0bc0b8f066e7eb57bfdc0bb6342785f8
romanlyakh <lyahroman@gmail.com>
2018-11-06 23:54:30 +0300

    Выделен класс-помощник для работы с классами. Выделен класс-помощник для работы с контактами.

---
+++ fixture/contact.py
@@ -0,0 +1,89 @@
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def add(self, contact):
+        wd = self.app.wd
+        # option add new
+        wd.find_element_by_link_text("add new").click()
+        #fio
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        #company details
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        # telephone number
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.work)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        #email
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact.email3)
+        #homepage
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        #Birthday
+        wd.find_element_by_name("bday").click()
+        wd.find_element_by_xpath(contact.bday).click()
+        wd.find_element_by_name("bmonth").click()
+        wd.find_element_by_xpath(contact.bmonth).click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        #Anniversary
+        wd.find_element_by_name("aday").click()
+        wd.find_element_by_xpath(contact.aday).click()
+        wd.find_element_by_name("amonth").click()
+        wd.find_element_by_xpath(contact.amonth).click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(contact.ayear)
+        # add address2
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.address2)
+        #add home
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact.phone2)
+        #add notes
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+        # save contact
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,27 +14,27 @@ def app(request):
 def test_add_contact(app):
         app.open_home_page()
         app.session.login(username="admin", password="secret")
-        app.add_contact(Contact(firstname="Петр", middlename="Анатольевич", lastname="Иванов", nickname="ivanov", title="test_title", company="company_test", address="address_test",
-                         home="+74951397247", mobile="+79061397247", work="+780080012014", fax="+780080012567", email="mail@gmail.com",
-                         email2="mail2@gmail.com", email3="mail3@gmail.com", homepage="ivanov.ru", byear="1980", ayear="2020", address2="address_test2",
-                         phone2="home_test", bday="//option[@value='11']", bmonth="//option[@value='January']", notes="notes_test",
-                         aday="(//option[@value='11'])[2]", amonth="(//option[@value='January'])[2]"))
-        app.return_home_page()
+        app.contact.add(Contact(firstname="Петр", middlename="Анатольевич", lastname="Иванов", nickname="ivanov", title="test_title", company="company_test", address="address_test",
+                                home="+74951397247", mobile="+79061397247", work="+780080012014", fax="+780080012567", email="mail@gmail.com",
+                                email2="mail2@gmail.com", email3="mail3@gmail.com", homepage="ivanov.ru", byear="1980", ayear="2020", address2="address_test2",
+                                phone2="home_test", bday="//option[@value='11']", bmonth="//option[@value='January']", notes="notes_test",
+                                aday="(//option[@value='11'])[2]", amonth="(//option[@value='January'])[2]"))
+        app.contact.return_home_page()
         app.session.logout()
 
 
 def test_add_contact_two(app):
         app.open_home_page()
         app.session.login(username="admin", password="secret")
-        app.add_contact(Contact(firstname="", middlename="", lastname="", nickname="",
-                                     title="", company="", address="",
-                                     home="", mobile="", work="",
-                                     fax="", email="",
-                                     email2="", email3="", homepage="",
-                                     byear="1980", ayear="2020", address2="address_test2",
-                                     phone2="home_test", bday="//option[@value='11']",
-                                     bmonth="//option[@value='January']", notes="notes_test",
-                                     aday="(//option[@value='11'])[2]", amonth="(//option[@value='January'])[2]"))
-        app.return_home_page()
+        app.contact.add(Contact(firstname="", middlename="", lastname="", nickname="",
+                                title="", company="", address="",
+                                home="", mobile="", work="",
+                                fax="", email="",
+                                email2="", email3="", homepage="",
+                                byear="1980", ayear="2020", address2="address_test2",
+                                phone2="home_test", bday="//option[@value='11']",
+                                bmonth="//option[@value='January']", notes="notes_test",
+                                aday="(//option[@value='11'])[2]", amonth="(//option[@value='January'])[2]"))
+        app.contact.return_home_page()
         app.session.logout()
 
