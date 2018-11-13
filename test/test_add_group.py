87ae33fb02706125c58503c0f69cd483ca58b56e
romanlyakh <lyahroman@gmail.com>
2018-11-14 01:06:45 +0300

    интеллектуальность фикстуры

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,20 +2,15 @@
 
 from model.group import Group
 
-
 def test_add_group(app):
     app.open_home_page()
-    #app.session.login(username="admin", password="secret")
     app.group.open_groups_page()
     app.group.create(Group(name="testname", header="testheader", footer="testfooter"))
     app.group.return_to_groups_page()
-    #app.session.logout()
 
 
 def test_add_empty_group(app):
     app.open_home_page()
-    #app.session.login(username="admin", password="secret")
     app.group.open_groups_page()
     app.group.create(Group(name="", header="", footer=""))
     app.group.return_to_groups_page()
-    #app.session.logout()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,13 +3,10 @@ from model.contact import Contact
 
 def test_modify_first_contact_firstname(app):
     app.open_home_page()
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="Святослав"))
-    app.session.logout()
+
 
 
 def test_modify_first_contact_lastname(app):
     app.open_home_page()
-   # app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(lastname="Смитт"))
-    app.session.logout()
