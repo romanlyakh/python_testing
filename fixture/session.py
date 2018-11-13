b24caca91c4d08770f4204f86a0f37796899792c
romanlyakh <lyahroman@gmail.com>
2018-11-14 02:35:28 +0300

    "интеллектуальность" функций login и logout

--- fixture/session.py
+++ fixture/session.py
@@ -4,7 +4,7 @@ class SessionHelper:
 
     def login(self, username, password):
         wd = self.app.wd
-        self.app.open_home_page()
+        #self.app.open_home_page()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
@@ -15,3 +15,26 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div[@id='top']/form/b").text=="("+username+")"
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        self.app.open_home_page()
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,8 +7,7 @@ def test_add_group(app):
     app.group.open_groups_page()
     app.group.create(Group(name="testname", header="testheader", footer="testfooter"))
     app.group.return_to_groups_page()
-
-
+    
 def test_add_empty_group(app):
     app.open_home_page()
     app.group.open_groups_page()
